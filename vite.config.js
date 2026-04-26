4afcac16ff10d5d4485e4cadeaed612b2864bfb1
Sabatouni <arefhimid@gmail.com>
2026-04-26 23:50:50 +0800

    updated vite config

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,9 @@
-import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
+import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
 
 export default defineConfig({
   plugins: [react()],
   build: {
-    // Increase the limit to 1000 KiB
-    chunkSizeWarningLimit: 1000,
-  },
-});
+    chunkSizeWarningLimit: 1000
+  }
+})
